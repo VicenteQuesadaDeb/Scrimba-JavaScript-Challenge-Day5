5b6d2d47db617c7570c3719ffa3007701f7cae19
Vicente Quesada Camón <vicentequesadadeb@gmail.com>
2020-06-06 15:32:42 +0200

    main.js solved

--- main.js
+++ main.js
@@ -1,38 +1,51 @@
 function evenDigitsOnly(number) {
-    //  write code here.
+  //  write code here.
+  const numberToString = number.toString();
+  const arrayNumbersString = numberToString.split("");
+  console.log(arrayNumbersString);
+  let arrayNumbersInt = [];
+  for (num of arrayNumbersString) {
+    arrayNumbersInt.push(parseInt(num));
+  }
+  console.log(arrayNumbersInt);
+  arrayNumbersInt.every(isEven);
+  console.log(arrayNumbersInt.every(isEven));
+  return arrayNumbersInt.every(isEven);
 }
 
-
+function isEven(num) {
+  return num % 2 === 0;
+}
 
 /**
-* Test Suite 
-*/
-describe('evenDigitsOnly()', () => {
-    it('returns true when all digits are even', () => {
-        // arrange
-        const nums = 248622;
-        
-        // act
-        const result = evenDigitsOnly(nums);
+ * Test Suite
+ */
+describe("evenDigitsOnly()", () => {
+  it("returns true when all digits are even", () => {
+    // arrange
+    const nums = 248622;
 
-        // log
-        console.log("result 1 : ", result);
-        
-        // assert
-        expect(result).toBe(true);
-    });
-    
-    it('returns fale when any digits are odd', () => {
-        // arrange
-        const nums = 642386;
-        
-        // act
-        const result = evenDigitsOnly(nums);
+    // act
+    const result = evenDigitsOnly(nums);
 
-        // log
-        console.log("result 2 : ", result);
-        
-        // assert
-        expect(result).toBe(false);
-    });
+    // log
+    console.log("result 1 : ", result);
+
+    // assert
+    expect(result).toBe(true);
+  });
+
+  it("returns fale when any digits are odd", () => {
+    // arrange
+    const nums = 642386;
+
+    // act
+    const result = evenDigitsOnly(nums);
+
+    // log
+    console.log("result 2 : ", result);
+
+    // assert
+    expect(result).toBe(false);
+  });
 });
